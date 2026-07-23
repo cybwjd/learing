62c8e14a38af2a55fe25cdbc075e6e810f1102f1
wjd <wajdm27@outlook.com>
2026-07-23 20:52:06 +0300

    Update View.py

--- View.py
+++ View.py
@@ -15,3 +15,16 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
 ]
+
+
+//core models.py
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='courses/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
