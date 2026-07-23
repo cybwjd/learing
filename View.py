4ced7e2d2435442bc7d44f9f9592b15f887d27e3
wjd <wajdm27@outlook.com>
2026-07-23 20:47:46 +0300

    Update View.py

--- View.py
+++ View.py
@@ -3,3 +3,15 @@ def login_view(request):
 
 def signup(request):
     return render(request, 'core/signup.html')
+
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('courses/', views.courses, name='courses'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup, name='signup'),
+]
