328a5314586078fb612194b47016d5f0b23acd38
wjd <wajdm27@outlook.com>
2026-07-23 22:37:17 +0300

    Update View.py

--- View.py
+++ View.py
@@ -43,3 +43,86 @@ admin.site.register(Course)
 <a href="{% url 'courses' %}">Courses</a>
 <a href="{% url 'login' %}">Login</a>
 <a href="{% url 'signup' %}">Sign Up</a>
+
+//login
+{% extends 'base.html' %}
+
+{% block title %}Login{% endblock %}
+
+{% block content %}
+
+<div class="container mt-5" style="max-width:500px;">
+
+    <h2 class="mb-4 text-center">Welcome Back 👋</h2>
+
+    <form>
+
+        <div class="mb-3">
+            <label>Email</label>
+            <input type="email" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label>Password</label>
+            <input type="password" class="form-control">
+        </div>
+
+        <button class="btn btn-primary w-100">
+            Login
+        </button>
+
+    </form>
+
+    <p class="text-center mt-3">
+        Don't have an account?
+        <a href="{% url 'signup' %}">Sign Up</a>
+    </p>
+
+</div>
+
+{% endblock %}
+
+
+//signup
+
+{% extends 'base.html' %}
+
+{% block title %}Sign Up{% endblock %}
+
+{% block content %}
+
+<div class="container mt-5" style="max-width:500px;">
+
+    <h2 class="mb-4 text-center">Create Your Account</h2>
+
+    <form>
+
+        <div class="mb-3">
+            <label>Full Name</label>
+            <input type="text" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label>Email</label>
+            <input type="email" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label>Password</label>
+            <input type="password" class="form-control">
+        </div>
+
+        <button class="btn btn-success w-100">
+            Create Account
+        </button>
+
+    </form>
+
+    <p class="text-center mt-3">
+        Already have an account?
+        <a href="{% url 'login' %}">Login</a>
+    </p>
+
+</div>
+
+{% endblock %}
